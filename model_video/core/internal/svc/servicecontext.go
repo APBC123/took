cb7c7deca45860daf34cc6fd74092526f4aa7a52
“APBC123” <2290312980@qq.com>
2023-02-10 18:46:48 +0800

    master

--- model_video/core/internal/svc/servicecontext.go
+++ model_video/core/internal/svc/servicecontext.go
@@ -2,14 +2,18 @@ package svc
 
 import (
 	"model_video/core/internal/config"
+	"model_video/core/models"
+	"xorm.io/xorm"
 )
 
 type ServiceContext struct {
 	Config config.Config
+	Engine *xorm.Engine
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
+		Engine: models.Init(c.Mysql.DataSource),
 	}
 }
